b337a878766a5b1a1d378ea2db565aee2ead3a7c
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-04 10:46:33 +0400

    installing next intl

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const withNextIntl = createNextIntlPlugin();
+ 
+/** @type {import('next').NextConfig} */
 const nextConfig = {};
+ 
+export default withNextIntl(nextConfig);
 
-export default nextConfig;
+
+
+// past edit 
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
