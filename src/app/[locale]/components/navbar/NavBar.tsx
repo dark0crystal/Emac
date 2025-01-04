5741a268316af723efb8896d170ada64f7818c5d
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-05 00:15:52 +0400

    navbar

---
+++ src/app/[locale]/components/navbar/NavBar.tsx
@@ -0,0 +1,27 @@
+import Brand from "./Brand";
+import NavLinks from "./NavLinks";
+import MobileBar from "./MobileBar";
+
+export default function NavBar() {
+  const navLinks = [
+    { direction: "/", name: "نبذة عنّا" },
+    { direction: "/", name: "خدماتنا" },
+    { direction: "/", name: "أعمالنا" },
+    { direction: "/", name: "تواصل معنا" },
+  ];
+
+  return (
+    <div className="flex justify-between items-center w-screen h-[10vh] bg-white shadow-lg shadow-gray-300 border-b-[1px] ">
+      {/* Brand Section */}
+      <Brand />
+
+      {/* Navigation Links (hidden on small screens) */}
+      <div className="hidden md:flex">
+        <NavLinks navLinks={navLinks} />
+      </div>
+
+      {/* Mobile Menu (visible on small screens) */}
+      <MobileBar navLinks={navLinks} />
+    </div>
+  );
+}
