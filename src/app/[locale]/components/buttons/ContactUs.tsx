54aa7f79d5f03b3103185277c8b09ad5d32f97e4
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-04 21:19:12 +0400

    styling button

---
+++ src/app/[locale]/components/buttons/ContactUs.tsx
@@ -0,0 +1,11 @@
+import { Link } from "@/i18n/routing";
+
+export default function ContactUs(){
+    return(
+        <div>
+            <div className="bg-violet-400 px-6 py-3 rounded-full">
+                <Link href="/about">إحجز موعد</Link>
+            </div>
+        </div>
+    )
+}
