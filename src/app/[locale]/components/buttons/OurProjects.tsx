f8c815419076952d69710cefde7bcc90378e7eeb
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-04 21:19:28 +0400

    add button

---
+++ src/app/[locale]/components/buttons/OurProjects.tsx
@@ -0,0 +1,11 @@
+import { Link } from "@/i18n/routing";
+
+export default function OurProjects(){
+    return(
+        <div>
+            <div className="bg-gray-400 px-6 py-3 rounded-full">
+                <Link href="/our-projects" >مشاهدة مشاريعنا</Link>
+            </div>
+        </div>
+    )
+}
