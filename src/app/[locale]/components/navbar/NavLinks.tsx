444b5da21efe2828695bcdb5df23a0a1dae38109
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-04 10:36:15 +0400

    adding nav links

---
+++ src/app/[locale]/components/navbar/NavLinks.tsx
@@ -0,0 +1,13 @@
+import { Link } from "@/i18n/routing";
+
+export default function NavLinks({ navLinks }:any) {
+  return (
+    <div className="flex items-center text-black mx-20">
+      {navLinks.map((navLink:any, index:any) => (
+        <Link href={navLink.direction} key={index}>
+          <h1 className="text-lg mx-4 font-normal">{navLink.name}</h1>
+        </Link>
+      ))}
+    </div>
+  );
+}
