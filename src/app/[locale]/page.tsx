7bb9dcee9042a393e5aa6a8f872a564ae87a23f1
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-05 11:47:37 +0400

    adding new components

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,16 +3,24 @@ import {useTranslations} from 'next-intl';
 import {Link} from '../../i18n/routing';
 import ImagesSlider from './components/Sliders/ImagesSlider';
 import Brand from './components/navbar/Brand';
+import ContactUsButton from './components/buttons/ContactUsButton';
+import OurProjectsButton from './components/buttons/OurProjectsButton';
+import Card1 from './components/Cards/Card1';
+import Card2 from './components/Cards/Card2';
+import Card3 from './components/Cards/Card3';
+import OurProjects from './components/our-projects/OurProjects';
+
  
 export default function HomePage() {
   const t = useTranslations('HomePage');
   return (
     <div>
-      <div className='h-[100vh]'>
+
+      {/* first section start  */}
+
+      <div className='h-screen'>
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
       <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_1000px_at_0%_100%,#0F75D5,transparent)]">
-      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to top, #0F75D5_1px, transparent_1px)]">
-
         <div className='grid grid-cols-2 h-full'>
           {/* left column */}
           <div className='px-20'>
@@ -20,20 +28,50 @@ export default function HomePage() {
           </div>
 
           {/* Right column */}
-          <div className='flex flex-col justify-center items-center text-right px-20'>
+          <div className='flex flex-col justify-center items-center text-right h-full px-20'>
             <div className=''>
               <h1 className='font-semibold leading-normal text-4xl'>{t('title')}</h1>
-              <Link href="/about">{t('about')}</Link>
-            </div>
-          </div>
-        </div>
+              <p className='mt-3 text-lg text-slate-800'>{t("titleParagraph")}</p>
+
+              <div className='flex flex-row justify-center gap-5 mt-7'> 
+                  <OurProjectsButton/>
+                  <ContactUsButton/>
+              </div>
+              
+               </div>
+              </div>
+             </div>
+           </div>
+           </div>
         
-          </div>
-          </div>
-          </div>
-          </div>
-     
-      <div className='h-[100vh]'></div>
+        </div>
+
+        {/* first section End  */}
+
+
+        <div className='h-[80vh]'>
+          <OurProjects/>
+        </div>
+        <div className='h-[70vh] bg-[#FFF6E9]'>
+          <h1 className='text-[#4335A7] text-3xl font-black'>إنطلق نحو السماء</h1>
+          <h1 className='text-[#FF7F3E] text-3xl font-black'>إنطلق نحو السماء</h1>
+          <h1 className='text-[#4335A7] text-3xl font-black'>إنطلق نحو السماء</h1>
+          <h1 className='text-[#FF7F3E] text-3xl font-black'>إنطلق نحو السماء</h1>
+          <h1 className='text-[#4335A7] text-3xl font-black'>إنطلق نحو السماء</h1>
+          <h1 className='text-[#FF7F3E] text-3xl font-black'>إنطلق نحو السماء</h1>
+          <h1 className='text-[#4335A7] text-3xl font-black'>إنطلق نحو السماء</h1>
+          <h1 className='text-[#FF7F3E] text-3xl font-black'>إنطلق نحو السماء</h1>
+        </div>
+
+     {/* why you should choose us Start*/}
+      <div className='h-[100vh] w-screen justify-center  items-center flex flex-col md:flex-row gap-4 md:flex-wrap'>
+        <Card1/>
+        <Card2/>
+        <Card3/>
+
+      </div>
+      {/* why you should choose us End */}
+
     </div>
   );
 }
