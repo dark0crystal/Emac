dc6aec2666f199a877a299f9fde7b39036d54a79
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-05 10:48:58 +0400

    card1 design

---
+++ src/app/[locale]/components/Cards/Card1.tsx
@@ -0,0 +1,12 @@
+export default function Card1(){
+    return(
+        <div className=" rounded-lg bg-white/55 border flex flex-col  h-[350px]   md:min-w-[400px]  md:h-[350px]  lg:w-[400px]   lg:h-[350px]   ">
+            <div className="top-4 right-4">
+                oman
+            </div>
+            <div>
+                <h1 className="text-black ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi voluptas velit rem officiis aliquam, ipsum repudiandae obcaecati incidunt, nam nisi, blanditiis architecto dolorem? Quod, obcaecati distinctio nam sequi est aliquam.</h1>
+            </div>
+        </div>
+    )
+}
