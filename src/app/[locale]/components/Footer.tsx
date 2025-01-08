5ab2bd542dfc05920ab786fc59103bb915ce4db3
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-09 00:10:39 +0400

    footer design

--- src/app/[locale]/components/Footer.tsx
+++ src/app/[locale]/components/Footer.tsx
@@ -1,21 +1,65 @@
 import { GrFanOption } from "react-icons/gr";
-export default function Footer(){
-    return(
-        <div className="h-[80vh] bg-[#F6F4F0] grid grid-rows-3">
-            <div>
-                <div className="h-1/3 bg-slate-600">
+import { TbCarFanFilled } from "react-icons/tb";
+import { MdOutlineElectricBolt } from "react-icons/md";
+import { LuHousePlug } from "react-icons/lu";
+import { MdElectricalServices } from "react-icons/md";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+import brand from "../../../../public/logo.png"
 
+
+
+export default function Footer() {
+    const t= useTranslations("footer")
+    return (
+        <div className="h-[60vh] bg-[#F6F4F0] grid grid-rows-3">
+            <div className=" flex justify-center items-center">
+                <div className="flex bg-gradient-to-r from-blue-400 via-blue-300 to-blue-300 shadow-lg shadow-slate-400 w-[70vw] rounded-3xl p-10 flex-col justify-center items-center">
+                    <div className="relative mx-10 md:mx-20 w-[80px] h-[80px] md:w-[120px] md:h-[120px]">
+                        <Image 
+                            alt="brand"
+                            src={brand}
+                            layout="fill"
+                            objectFit="contain"
+                        />
+                    </div>
+                    <div>
+                        <p className="text-black text-center">{t("CompanyfullName")}</p>
+                    </div>
                 </div>
-                <div className="h-1/3 bg-emerald-400">
-                    
-                </div>
-                <div className="flex h-1/3 bg-orange-300">
-    <GrFanOption className="w-60 h-60 sm:w-9 sm:h-9 lg:w- lg:h-12" />
-    <GrFanOption className="w-6 h-6 sm:w-9 sm:h-9 lg:w-12 lg:h-12" />
-    <GrFanOption className="w-6 h-6 sm:w-9 sm:h-9 lg:w-12 lg:h-12" />
-</div>
 
             </div>
+            {/*  */}
+            <div className=" flex flex-col md:flex-row justify-center">
+                {/* copyrights */}
+                <div className=" flex justify-center items-center mx-8">
+                    <h1 className="mx-1 text-black/70">
+                    <span className="font-bold text-lg text-black/40">. </span>
+                        {t("copyright")} 
+                    </h1>
+                    <h1 className="font-semibold text-[#4335A7]/80 mx-1">
+                        {t("companyName")} 
+                    </h1>
+                 </div>
+                 {/* designer */}
+                <div className=" flex justify-center items-center ">
+                    <h1 className="mx-1 text-black/70">
+                        <span className="font-bold text-lg text-black/40">. </span>
+                        {t("designedBy")} 
+                    </h1>
+                    <h1 className="font-semibold text-[#4335A7]/80 mx-1">
+                        {t("designerName")}
+                    </h1>
+                 </div>
+                 
+                 
+            </div>
+            <div className=" flex justify-around items-center ">
+                <MdOutlineElectricBolt className="w-16 h-16 md:w-28 md:h-28 lg:w-36 lg:h-36 text-black" />
+                <LuHousePlug className="w-16 h-16 md:w-28 md:h-28 lg:w-36 lg:h-36 mx-4 text-black" />
+                <MdElectricalServices className="w-16 h-16 md:w-28 md:h-28 lg:w-36 lg:h-36 text-black" />
+                <TbCarFanFilled className="w-16 h-16 md:w-28 md:h-28 lg:w-36 lg:h-36 text-black" />
+            </div>
         </div>
-    )
+    );
 }
