980004eea8aabdc4ead13f07abe796dd537af136
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-09 00:24:02 +0400

    service cards

--- src/app/[locale]/components/our-services/OurServices.tsx
+++ src/app/[locale]/components/our-services/OurServices.tsx
@@ -1,8 +1,27 @@
+import Image from "next/image"
+import img1 from "../../../../../public/img1.jpeg"
+import img2 from "../../../../../public/img2.jpeg"
+import img3 from "../../../../../public/img3.jpeg"
+
 export default function OurServices(){
+    const services =[
+        {title:"service 1" ,describtion:"its a good service", img:img1},
+        {title:"service 1" ,describtion:"", img:img2},
+        {title:"service 1" ,describtion:"", img:img3},
+        
+    ]
     return(
         <div>
             <div>
-                
+            {
+                services.map((service,index)=>(
+                    <div key={index}>
+                        <h1>{service.title}</h1>
+                        <p>{service.describtion}</p>
+                        <Image src={service.img} alt="service img"/>
+                    </div>
+                ))
+            }
             </div>
         </div>
     )
