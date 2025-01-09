48c3935fd5e4bf19d471eb8f163b8941b790330e
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-09 21:59:37 +0400

    adding text about us

--- src/app/[locale]/components/about/AboutUs.tsx
+++ src/app/[locale]/components/about/AboutUs.tsx
@@ -1,8 +1,18 @@
+import { useTranslations } from "next-intl"
 export default function About(){
+    const t= useTranslations("about")
     return(
-        <div>
-            <h1 className="bg-violet-300 ">lahg lasdjhgs agjh</h1>
-            About
+        <div className="flex  items-center justify-center  w-screen md:mt-6  h-[70vh] md:px-[8vw] lg:px-[12vw]">
+            <div className="w-[95vw] md:w-full ">
+                <div className="mt-4 text-center">
+                    <h1 className="text-2xl md:text-4xl font-bold ">{t("title")}</h1>
+                    <div className="h-[2px] w-full bg-black mt-6"></div>
+                </div>
+                <div className="my-8 text-center">
+                    <p className="text-xl md:text-2xl ">{t("description")}</p>
+                </div>
+            </div>
+        
         </div>
     )
 }
