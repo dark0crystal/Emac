1218e840f4eccf0905f746d0ea5fce8a351099e7
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-10 11:22:43 +0400

    fix sizing

--- src/app/[locale]/components/buttons/ContactUsButton.tsx
+++ src/app/[locale]/components/buttons/ContactUsButton.tsx
@@ -3,8 +3,8 @@ import { Link } from "@/i18n/routing";
 export default function ContactUsButton(){
     return(
         <div>
-            <div className="bg-violet-400 px-6 py-3 rounded-full">
-                <Link href="/about" className="text-lg font-normal">احجز موعد</Link>
+            <div className="bg-violet-400/70 px-8 py-3 rounded-full">
+                <Link href="/about" className="text-xl font-normal">احجز موعد</Link>
             </div>
         </div>
     )
