d70160c60b7b11c64e93118cadf0d6c0751169e6
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-10 10:05:29 +0400

    fix

--- src/app/[locale]/components/our-services/OurServices.tsx
+++ src/app/[locale]/components/our-services/OurServices.tsx
@@ -13,7 +13,7 @@ export default function OurServices() {
     ];
 
     return (
-        <div className="h-fit w-screen  flex flex-col items-center p-10">
+        <div className="h-fit w-screen  flex flex-col items-center p-10 my-24">
             <div>
                 <h1 className="text-2xl md:text-4xl font-bold text-center my-12">{t("title")}</h1>
             </div>
@@ -29,7 +29,7 @@ export default function OurServices() {
                         objectFit="cover"
                         className="absolute inset-0 z-0"
                     />
-                    <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent z-10"></div>
+                    <div className="absolute inset-0 bg-gradient-to-t from-sky-800/80 to-transparent z-10"></div>
                     <div className="relative z-20 h-full flex flex-col justify-end p-5 text-white">
                         <h1 className="text-2xl font-bold mb-2">{service.title}</h1>
                         <p className="text-sm">{service.description}</p>
