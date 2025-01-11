fd8f55effde50041b61f2a18d7895c9d488ce8d4
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-11 19:12:36 +0400

    translation

--- src/app/[locale]/components/navbar/LanguageChange.tsx
+++ src/app/[locale]/components/navbar/LanguageChange.tsx
@@ -2,12 +2,13 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { useTranslations, useLocale } from "next-intl";
+
 // import { Inter } from "next/font/google";
 
 // const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] ,display: 'swap',});
 
 export default function LanguageChange() {
-    const t = useTranslations("Links");
+    const t = useTranslations("navbar");
     const pathName = usePathname();
     const locale = useLocale();
 
@@ -17,12 +18,12 @@ export default function LanguageChange() {
     return (
         <div >
             {locale === "ar" ? (
-                <Link className="mx-6 text-3xl font-bold" href={`/en${pathWithoutLocale}`} locale="en">
-                    english
+                <Link className="mx-6 text-xl font-normal" href={`/en${pathWithoutLocale}`} locale="en">
+                    {t("langChange")}
                 </Link>
             ) : (
-                <Link className="mx-6 text-3xl font-bold" href={`/ar${pathWithoutLocale}`} locale="ar">
-                    arabic
+                <Link className="mx-6 text-xl font-normal" href={`/ar${pathWithoutLocale}`} locale="ar">
+                   {t("langChange")}
                 </Link>
             )}
         </div>
