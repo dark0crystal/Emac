c184bf8eb75267c5b995efe0c4d9396139a73c4a
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-11 22:59:27 +0400

    our clients glitching fix

--- src/app/[locale]/components/our-clients/OurClients.tsx
+++ src/app/[locale]/components/our-clients/OurClients.tsx
@@ -1,28 +1,52 @@
+"use client"
 import { MotionDiv } from "../MotionDiv"
 import img1 from "../../../../../public/img1.jpeg"
 import img2 from "../../../../../public/img2.jpeg"
 import img3 from "../../../../../public/img3.jpeg"
 import img4 from "../../../../../public/img4.jpeg"
 import img5 from "../../../../../public/img5.jpeg"
+import img6 from "../../../../../public/imag17.png"
 import Card from "./Card"
+import { useRef ,useEffect } from "react"
+import { animate, useMotionValue,motion } from "framer-motion"
+import useMeasure from "react-use-measure"
 
 const CardContent =[
-                    {title:"almoug" ,image:img1},
-                    {title:"almoug" ,image:img2},
-                    {title:"almoug" ,image:img3},
-                    {title:"almoug" ,image:img4},
-                    {title:"almoug" ,image:img5},
-
+                    {title:"almoug" ,image:img6},
+                    {title:"almoug" ,image:img6},
+                    {title:"almoug" ,image:img6},
+                    {title:"almoug" ,image:img6},
+                    {title:"almoug" ,image:img6},
+                    
                 ];
 
 export default function OurClients(){
+
+    let [ref ,{width}] = useMeasure();
+    const xTranslation =useMotionValue(0);
+
+    useEffect(()=>{
+        let controls;
+        let finalPosition = - width/3;
+
+        controls = animate(xTranslation , [0,finalPosition],{
+            ease:"linear",
+            duration:10,
+            repeat:Infinity,
+            repeatType:"loop",
+            repeatDelay:0,
+            
+        });
+        return controls.stop;
+    },[xTranslation,width])
     return(
-        <div>
-            <div className="flex">
-                {CardContent.map((content,index)=>(
+        <div dir="ltr">
+            <motion.div className="flex gap-16 w-max" ref={ref} style={{x:xTranslation}}>
+                {/* dublicate the cards  */}
+                {[...CardContent , ...CardContent ,...CardContent].map((content,index)=>(
                     <Card key={index} title={content.title} image={content.image}/>
                 ))}
-            </div>
+            </motion.div>
         </div>
     )
 }
