f6fbf5b3a1baabf772a4184545d11e8bb9ded409
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-11 19:12:45 +0400

    translation

--- src/app/[locale]/components/navbar/NavBar.tsx
+++ src/app/[locale]/components/navbar/NavBar.tsx
@@ -1,18 +1,18 @@
 import Brand from "./Brand";
 import NavLinks from "./NavLinks";
 import MobileBar from "./MobileBar";
-import LanguageChange from "./LanguageChange";
-
+import { useTranslations } from "next-intl";
 export default function NavBar() {
+  const t= useTranslations("navbar");
   const navLinks = [
-    { direction: "/", name: "نبذة عنّا" },
-    { direction: "/", name: "خدماتنا" },
-    { direction: "/", name: "أعمالنا" },
-    { direction: "/", name: "تواصل معنا" },
+    { direction: "#about-us", name:t("about-us")},
+    { direction: "#our-services", name: t("our-services") },
+    { direction: "#our-projects", name: t("our-projects")},
+    { direction: "#contact-us", name: t("contact-us")},
   ];
 
   return (
-    <div dir="rtl"  className="flex justify-between items-center w-screen h-[10vh] bg-white/40  border-b-[1px] ">
+    <div   className="flex justify-between items-center w-screen h-[10vh] bg-white/40  border-b-[1px] ">
       
 
       {/* Navigation Links (hidden on small screens) */}
