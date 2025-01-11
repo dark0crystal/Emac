a81033e1542ab255a73833ee30192a802a70bac0
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-11 23:00:01 +0400

    fixing image slider finite

--- src/app/[locale]/components/Sliders/ImagesSlider.tsx
+++ src/app/[locale]/components/Sliders/ImagesSlider.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { motion, useMotionValue ,animate } from "framer-motion";
 import img1 from "../../../../../public/img1.jpeg"
 import img2 from "../../../../../public/img2.jpeg"
 import img3 from "../../../../../public/img3.jpeg"
 import img4 from "../../../../../public/img4.jpeg"
 import img5 from "../../../../../public/img5.jpeg"
+import {useRef ,useEffect} from "react"
+import useMeasure from "react-use-measure";
 
 export default function ImagesSlider() {
   const cards = [
@@ -14,7 +16,28 @@ export default function ImagesSlider() {
     { id: 3 ,img: img3},
     { id: 4 ,img: img4},
     { id: 5 ,img: img5},
+    { id: 4 ,img: img4},
+    { id: 5 ,img: img5},
   ];
+
+  let [ref ,{height}] = useMeasure();
+  // const xTranslation =useMotionValue(0);
+
+  let yTranslation = useMotionValue(0);
+  useEffect(()=>{
+    let controls;
+    let finalPosition = - height/4;
+
+    controls = animate(yTranslation , [0,finalPosition],{
+        ease:"linear",
+        duration:25,
+        repeat:Infinity,
+        repeatType:"loop",
+        repeatDelay:0,
+        
+    });
+    return controls.stop;
+},[yTranslation,height])
 
   return (
     <div className="relative h-full overflow-hidden flex flex-col items-center ">
@@ -23,21 +46,15 @@ export default function ImagesSlider() {
 
       
       <motion.div
-        className="absolute flex flex-col "
-        initial={{ y: 0 }}
-        animate={{ y: "-100%" }}
-        transition={{
-          repeat: Infinity,
-          duration: 30, // Adjust speed by changing duration
-          ease: "linear",
-        }}
+        className=" h-max gap-4"
+        ref={ref} style={{y:yTranslation}}
       >
         
         {/* Original Cards */}
-        {cards.map((card) => (
+        {[...cards , ...cards ].map((card) => (
           <div
             key={card.id}
-            className="bg-white relative shadow-lg rounded-sm my-2  overflow-hidden min-h-72 min-w-[90vw] lg:min-h-[340px] lg:min-w-[540px] mx-auto"
+            className="bg-white relative shadow-lg rounded-sm my-2  overflow-hidden min-h-72 min-w-[90vw] lg:min-h-[340px] lg:min-w-[540px] "
           >
             <Image
              alt="img"
