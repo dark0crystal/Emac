4299503ba0e3d1d7fea5957abfaa6184aa3d9371
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-12 10:50:54 +0400

    contact us translated

--- src/app/[locale]/components/buttons/ContactUsButton.tsx
+++ src/app/[locale]/components/buttons/ContactUsButton.tsx
@@ -1,10 +1,11 @@
 import { Link } from "@/i18n/routing";
-
+import { useTranslations } from "next-intl";
 export default function ContactUsButton(){
+    const t=useTranslations("HomePage")
     return(
         <div>
             <div className="bg-violet-400/70 px-8 py-3 rounded-full">
-                <Link href="/about" className="text-xl font-normal">احجز موعد</Link>
+                <Link href="/about" className="text-xl font-normal">{t("contactUs")}</Link>
             </div>
         </div>
     )
