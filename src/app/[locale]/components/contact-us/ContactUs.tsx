8144d36dff764737afa0b8ae357d51a4b6a502fb
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-13 08:50:01 +0400

    adding animation

--- src/app/[locale]/components/contact-us/ContactUs.tsx
+++ src/app/[locale]/components/contact-us/ContactUs.tsx
@@ -3,6 +3,7 @@ import gmailIcon from "../../../../../public/gmail.png"
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { FaWhatsapp } from "react-icons/fa";
+import { MotionDiv } from "../MotionDiv";
 
 export default function ContactUs() {
   const t = useTranslations("contact-us");
@@ -10,12 +11,18 @@ export default function ContactUs() {
   return (
     <div className="h-[50vh] text-center p-8 mt-20">
       {/* Title */}
-      <div className="flex flex-col items-center">
+      <MotionDiv
+       className="flex flex-col items-center"
+       initial={{ opacity: 0, y: 150 }} // Initial state
+            whileInView={{ opacity: 1, y: 0 }} // Animation while in view
+            viewport={{ once: true, amount: 0.2 }} // Trigger once when 20% in view
+            transition={{ duration: 0.8, ease: "easeOut" }} // Animation timing
+       >
             <h2 className="relative z-20 text-2xl md:text-4xl font-bold text-center my-12">
             {t("title")}
                 <span className="absolute -z-10 bottom-0 left-1/2 transform -translate-x-1/2 w-full h-4 md:h-5 bg-blue-400/40"></span>
             </h2>
-      </div>
+      </MotionDiv>
 
 
       <div className="flex flex-row gap-8 md:gap-32 justify-center items-center">
